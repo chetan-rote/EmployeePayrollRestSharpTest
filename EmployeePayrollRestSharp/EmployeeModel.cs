3a43d5437d45f4d372ca53fab541d015e36b8b5c
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-01 14:10:47 +0530

    Add project files.

---
+++ EmployeePayrollRestSharp/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollRestSharp
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+}
